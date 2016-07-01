5fe70b759ec536c5bbc826700c1dbb05a7b6e9de
swaption2009 <swaption2009@gmail.com>
2016-07-01 16:37:19 -0700

    udadicity tests
    - create singleton class
    - pass create, all, first(n) tests

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -3,5 +3,44 @@ require_relative 'errors'
 require 'csv'
 
 class Udacidata
-  # Your code goes here!
+  class << self
+
+    DATA_PATH = File.dirname(__FILE__) + "/../data/data.csv"
+
+    def create(args={})
+      product = self.new(args)
+      append_csv(product)
+      product
+    end
+
+    def all
+      @arrays_of_products = []
+      read_csv
+      @arrays_of_products
+    end
+
+    def first(n = nil)
+      n ||= 1
+      if n > 1
+        array = CSV.read(DATA_PATH)[1, n]
+      else
+        self.new
+      end
+    end
+
+    private
+
+    def append_csv(product)
+      CSV.open(DATA_PATH, "a+") do |csv|
+        csv << [product.id, product.brand, product.name, product.price]
+      end
+    end
+
+    def read_csv
+      CSV.foreach(DATA_PATH, headers: true) do |row|
+        @arrays_of_products << self.new(brand: row[1], name: row[2], price: row[3].to_i)
+      end
+    end
+
+  end
 end
