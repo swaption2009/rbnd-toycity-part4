35fc4c7ce34fa0f33407c10dcac93fe96e3cbdbd
swaption2009 <swaption2009@gmail.com>
2016-07-03 11:27:47 -0700

    2nd commit
    - first(n) method
    - last and last(n) method
    - find(n) method

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -5,40 +5,78 @@ require 'csv'
 class Udacidata
   class << self
 
-    DATA_PATH = File.dirname(__FILE__) + "/../data/data.csv"
+    DATA_PATH = File.expand_path("..", Dir.pwd) + "/data/data.csv"
 
     def create(args={})
       product = self.new(args)
-      append_csv(product)
+      # store to database
+      CSV.open(DATA_PATH, "a+") do |csv|
+        csv << [product.id, product.brand, product.name, product.price]
+      end
+      # return product object
       product
     end
 
     def all
       @arrays_of_products = []
-      read_csv
-      @arrays_of_products
+      read_csv # get all records in database
+      @arrays_of_products # return array of products
     end
 
-    def first(n = nil)
-      n ||= 1
-      if n > 1
-        array = CSV.read(DATA_PATH)[1, n]
-      else
-        self.new
+    def first
+      data = Array.new
+      CSV.foreach(DATA_PATH, { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+        data << row.to_hash
       end
+      n = 0
+      self.new(id: data[n][:id], brand: data[n][:brand], name: data[n][:product], price: data[n][:price])
+      # product = self.new(id: data[n][:id], brand: data[n][:brand], name: data[n][:product], price: data[n][:price])
+      # puts product.id
     end
 
-    private
-
-    def append_csv(product)
-      CSV.open(DATA_PATH, "a+") do |csv|
-        csv << [product.id, product.brand, product.name, product.price]
+    def first(n)
+      data = Array.new
+      CSV.foreach(DATA_PATH, { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+        data << row.to_hash
       end
+      @arrays_of_first_n_products = Array.new
+      data[0..n-1].each do |data|
+        @arrays_of_first_n_products << self.new(id: data[:id], brand: data[:brand], name: data[:product], price: data[:price])
+      end
+      @arrays_of_first_n_products
     end
 
-    def read_csv
-      CSV.foreach(DATA_PATH, headers: true) do |row|
-        @arrays_of_products << self.new(brand: row[1], name: row[2], price: row[3].to_i)
+    def last
+      data = Array.new
+      CSV.foreach(DATA_PATH, { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+        data << row.to_hash
+      end
+      n = -1
+      self.new(id: data[n][:id], brand: data[n][:brand], name: data[n][:product], price: data[n][:price])
+    end
+
+    def last(n)
+      data = Array.new
+      CSV.foreach(DATA_PATH, { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+        data << row.to_hash
+      end
+      @arrays_of_last_n_products = Array.new
+      data[-n..-1].each do |data|
+        @arrays_of_last_n_products << self.new(id: data[:id], brand: data[:brand], name: data[:product], price: data[:price])
+      end
+      @arrays_of_last_n_products
+    end
+
+    def find(n)
+      data = Array.new
+      CSV.foreach(DATA_PATH, { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+        data << row.to_hash
+      end
+      data.each do |data|
+        if data[:id] == 5
+          data = self.new(id: data[:id], brand: data[:brand], name: data[:product], price: data[:price])
+          return data
+        end
       end
     end
 
