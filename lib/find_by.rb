b650cb8be363600285e56d1c549b745b377f0ea7
swaption2009 <swaption2009@gmail.com>
2016-07-04 21:16:37 -0700

    complete method based on tests
    - No Implicit conversion of nil to String ???
    - # Undefined method `name' for #<Array:>

--- lib/find_by.rb
+++ lib/find_by.rb
@@ -1,6 +1,36 @@
 class Module
-  def create_finder_methods(*attributes)
-    # Your code goes here!
-    # Hint: Remember attr_reader and class_eval
+  attr_reader :brand, :name
+
+  DATA_PATH = File.expand_path("..", Dir.pwd) + "/data/data.csv"
+
+  def method_missing(method_name, *args, &block)
+    name = method_name.to_s.downcase
+    if(match_data = /^find_by_()(\w*?)?$/.match name)
+      create_finder_methods match_data[2]
+      send(method_name, *args)
+    else
+      super(method_name, *args)
+    end
+  end
+
+ def create_finder_methods(*attributes)
+    attributes.each do |attribute|
+      find_by = %Q{
+        def self.find_by_#{attribute}(n)
+          data = Array.new
+          CSV.foreach(DATA_PATH, { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+            data << row.to_hash
+          end
+          data.each do |data|
+            if data[:#{attribute}] == n
+            data = self.new(id: data[:id], brand: data[:brand], name: data[:product], price: data[:price])
+            return data
+          end
+        end
+      end
+      }
+      puts find_by
+      class_eval(find_by)
+    end
   end
 end
--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -2,7 +2,23 @@ require_relative 'find_by'
 require_relative 'errors'
 require 'csv'
 
-class Udacidata
+class Udacidata < Module
+
+  def update(opts = {})
+    @id = self.id # keep same id
+    @brand = opts[:brand] || self.brand
+    @name = opts[:name] || self.name
+    @price = opts[:price] || self.price
+    # destroy current csv record
+    self.class.destroy(@id)
+    # append new record into csv
+    CSV.open(DATA_PATH, "a+") do |csv|
+      csv << [@id, @brand, @name, @price]
+    end
+    # return object
+    return self
+  end
+
   class << self
 
     DATA_PATH = File.expand_path("..", Dir.pwd) + "/data/data.csv"
@@ -24,60 +40,111 @@ class Udacidata
     end
 
     def first
+      # get CSV and convert to hash
       data = Array.new
       CSV.foreach(DATA_PATH, { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
         data << row.to_hash
       end
-      n = 0
+      n = 0 # get the first record in hashed data
+      # create and return object from first hashed data
       self.new(id: data[n][:id], brand: data[n][:brand], name: data[n][:product], price: data[n][:price])
-      # product = self.new(id: data[n][:id], brand: data[n][:brand], name: data[n][:product], price: data[n][:price])
-      # puts product.id
     end
 
     def first(n)
+      # get CSV and convert to hash
       data = Array.new
       CSV.foreach(DATA_PATH, { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
         data << row.to_hash
       end
+      # create and return object from first n hashed data
       @arrays_of_first_n_products = Array.new
       data[0..n-1].each do |data|
         @arrays_of_first_n_products << self.new(id: data[:id], brand: data[:brand], name: data[:product], price: data[:price])
       end
+      # return array of n products
       @arrays_of_first_n_products
     end
 
     def last
+      # get CSV and convert to hash
       data = Array.new
       CSV.foreach(DATA_PATH, { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
         data << row.to_hash
       end
-      n = -1
+      n = -1 # get the last record in hashed data
+      # create and return object from first hashed data
       self.new(id: data[n][:id], brand: data[n][:brand], name: data[n][:product], price: data[n][:price])
     end
 
     def last(n)
+      # get CSV and convert to hash
       data = Array.new
       CSV.foreach(DATA_PATH, { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
         data << row.to_hash
       end
+      # create and return object from last n hashed data
       @arrays_of_last_n_products = Array.new
       data[-n..-1].each do |data|
         @arrays_of_last_n_products << self.new(id: data[:id], brand: data[:brand], name: data[:product], price: data[:price])
       end
+      # return array of n products
       @arrays_of_last_n_products
     end
 
     def find(n)
+      # get CSV and convert to hash
       data = Array.new
       CSV.foreach(DATA_PATH, { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
         data << row.to_hash
       end
+      # match hashed data with id number
       data.each do |data|
         if data[:id] == 5
           data = self.new(id: data[:id], brand: data[:brand], name: data[:product], price: data[:price])
-          return data
+          return data # return object with id number == n
         end
       end
+    end
+
+    def destroy(n)
+      # get CSV and convert to hash
+      data = Array.new
+      CSV.foreach(DATA_PATH, { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+        data << row.to_hash
+      end
+      # delete from CSV
+      table = CSV.table(DATA_PATH)
+      table.delete_if do |row|
+        row[:id] == n
+      end
+      File.open(DATA_PATH, 'w') do |f|
+        f.write(table.to_csv)
+      end
+      # return destroyed object
+      data.each do |data|
+        if data[:id] == n
+          destroy = self.new(id: data[:id], brand: data[:brand], name: data[:product], price: data[:price])
+          return destroy
+        end
+      end
+    end
+
+    def where(options = {})
+      # get CSV and convert to hash
+      data = Array.new
+      CSV.foreach(DATA_PATH, { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+        data << row.to_hash
+      end
+      selected_products = Array.new
+      # search hashed data and match with options parameters
+      data.each do |data|
+        options.each do |key, value|
+         if data[:key.to_s] == value.to_s
+            selected_products << data
+          end
+        end
+      end
+      return selected_products # return array of selected products
     end
 
   end
