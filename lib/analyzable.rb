97b0175cd3dd0b9d1b4cbcad7e440f9bf23e968c
swaption2009 <swaption2009@gmail.com>
2016-07-07 13:27:43 -0700

    pass analyzable_test

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -1,15 +1,37 @@
+require 'colorize'
+
 module Analyzable
   # Your code goes here!
   def average_price(products)
-    p products
+    sum = 0
+    products.each do |product|
+      sum += product.price
+    end
+    return ( sum / products.count ).to_f.round(2)
   end
 
-  def print_report
+  def print_report(products)
+    report = Hash.new(0)
+    puts "Inventory Report"
+    products.each do |product|
+      puts "brand: #{product.brand.red}, product: #{product.name.yellow}, price: $#{product.price.to_s.blue}"
+    end
+    return report.to_s
   end
 
-  def count_by_brand
+  def count_by_brand(products)
+    brand = Hash.new(0)
+    products.each do |product|
+      brand[product.brand] += 1
+    end
+    return brand
   end
 
-  def count_by_name
+  def count_by_name(products)
+    name = Hash.new(0)
+    products.each do |product|
+      name[product.name] += 1
+    end
+    return name
   end
 end
